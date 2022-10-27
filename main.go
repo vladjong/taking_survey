1559326fd015696e26fc6147cfe7de6894a7a022
vladjong <va.gaidenko@gmail.com>
2022-10-27 20:45:04 +0700

    Реализован механизм прохождения теста

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/spf13/viper"
@@ -15,22 +20,36 @@ import (
 type Client struct {
 	Client         http.Client
 	NumberQuestion int
-	Answers        map[string]string
 }
 
 func main() {
 	if err := InitConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
+	ctx := context.Background()
 	client := NewClinet()
-	if err := client.getFirstLink(); err != nil {
+	_, cookies, err := client.GetPage(ctx, http.MethodGet, viper.GetString("url"), nil, nil, nil, 10)
+	if err != nil {
 		log.Fatalf("error get first link: %s", err.Error())
 	}
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/x-www-form-urlencoded"
+	doc, _, err := client.GetPage(ctx, http.MethodGet, client.getLink(), cookies, headers, nil, 10)
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
 	for {
-		client.takingSurvey()
+		formDatas := client.parseForm(doc)
+		doc, _, err = client.GetPage(ctx, http.MethodPost, client.getLink(), cookies, headers, formDatas, 10)
+		if err != nil {
+			log.Fatalf("error: %s", err.Error())
+		}
+		if strings.Contains(doc.Text(), "Test successfully passed") {
+			fmt.Println("ПРОШЕЛ ТЕСТ")
+			return
+		}
 		client.NumberQuestion += 1
 	}
-	fmt.Println(client.Answers)
 }
 
 func InitConfig() error {
@@ -44,31 +63,7 @@ func NewClinet() *Client {
 	return &Client{
 		Client:         http.Client{Jar: cookieJar},
 		NumberQuestion: 1,
-		Answers:        make(map[string]string),
 	}
-}
-
-func (c *Client) getFirstLink() error {
-	resp, err := c.Client.Get(viper.GetString("url"))
-	if err != nil {
-		log.Fatalf("error get url: %s", err.Error())
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
-	}
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatalf("error initializing goquery response body: %s", err.Error())
-	}
-	link, ok := doc.Find("a").Attr("href")
-	if !ok {
-		log.Fatal(err)
-	}
-	if link == c.getPath() {
-		return nil
-	}
-	return fmt.Errorf("error this is incorect page")
 }
 
 func (c *Client) getPath() string {
@@ -79,47 +74,35 @@ func (c *Client) getLink() string {
 	return viper.GetString("url") + c.getPath()
 }
 
-func (c *Client) takingSurvey() {
-	resp, err := c.Client.Get(c.getLink())
-	if err != nil {
-		log.Fatalf("error get url: %s", err.Error())
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
-	}
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatalf("error initializing goquery response body: %s", err.Error())
-	}
+func (c *Client) parseForm(doc *goquery.Document) map[string]string {
+	formDatas := make(map[string]string)
 	form := doc.Find("form")
 	form.Find("p").Each(func(i int, s *goquery.Selection) {
-		c.parseSelect(s)
-		c.parseType(s)
+		c.parseSelect(s, formDatas)
+		c.parseType(s, formDatas)
 	})
-	fmt.Println(c.Answers)
+	return formDatas
 }
 
-func (c *Client) parseSelect(s *goquery.Selection) {
+func (c *Client) parseSelect(s *goquery.Selection, formDatas map[string]string) {
 	s.Find("select").Each(func(i int, s *goquery.Selection) {
 		name, ok := s.Attr("name")
 		if name == "" || !ok {
 			return
 		}
-		var temp string
 		s.Find("option").Each(func(i int, s *goquery.Selection) {
 			value, ok := s.Attr("value")
 			if !ok {
 				return
 			}
-			maxString(value, &temp)
+			if len(value) > len(formDatas[name]) {
+				formDatas[name] = value
+			}
 		})
-		c.Answers[name] = temp
 	})
 }
 
-func (c *Client) parseType(s *goquery.Selection) {
-	var temp string
+func (c *Client) parseType(s *goquery.Selection, formDatas map[string]string) {
 	s.Find("input").Each(func(i int, s *goquery.Selection) {
 		name, ok := s.Attr("name")
 		if name == "" || !ok {
@@ -129,15 +112,16 @@ func (c *Client) parseType(s *goquery.Selection) {
 		if !ok {
 			return
 		}
-		if typ == "radio" {
+		if typ == "text" {
+			formDatas[name] = "test"
+		} else if typ == "radio" {
 			value, ok := s.Attr("value")
 			if !ok {
 				return
 			}
-			maxString(value, &temp)
-			c.Answers[name] = temp
-		} else if typ == "text" {
-			c.Answers[name] = "text"
+			if len(value) > len(formDatas[name]) {
+				formDatas[name] = value
+			}
 		}
 	})
 }
@@ -146,4 +130,49 @@ func maxString(value string, temp *string) {
 	if len(value) > len(*temp) {
 		*temp = value
 	}
+}
+
+func (c *Client) GetPage(ctx context.Context, method, siteURL string, cookies []*http.Cookie, headers, formDatas map[string]string, timeout int) (*goquery.Document, []*http.Cookie, error) {
+	body := io.Reader(nil)
+	if len(formDatas) > 0 {
+		form := url.Values{}
+		for k, v := range formDatas {
+			form.Add(k, v)
+		}
+		body = strings.NewReader(form.Encode())
+	}
+	req, err := http.NewRequestWithContext(ctx, method, siteURL, body)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create http request context: %w", err)
+	}
+	if len(headers) > 0 {
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+	}
+	if len(cookies) > 0 {
+		for _, c := range cookies {
+			req.AddCookie(c)
+		}
+	}
+	reqTimeout := 10 * time.Second
+	if timeout != 0 {
+		reqTimeout = time.Duration(timeout) * time.Second
+	}
+	httpClient := &http.Client{
+		Transport:     http.DefaultTransport,
+		CheckRedirect: nil,
+		Jar:           nil,
+		Timeout:       reqTimeout,
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to execute http request: %w", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to parse html: %w", err)
+	}
+	return doc, resp.Cookies(), nil
 }
